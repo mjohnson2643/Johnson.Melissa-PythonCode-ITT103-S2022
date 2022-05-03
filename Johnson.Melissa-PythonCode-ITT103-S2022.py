09a8de6954184adb39af0b7b495add87c55a9b1e
mjohnson2643 <104771758+mjohnson2643@users.noreply.github.com>
2022-05-03 17:05:37 -0500

    Update Johnson.Melissa-PythonCode-ITT103-S2022.py

--- Johnson.Melissa-PythonCode-ITT103-S2022.py
+++ Johnson.Melissa-PythonCode-ITT103-S2022.py
@@ -2,6 +2,11 @@
 #Date Created:April-25- 2022
 # Course: ITT103
 # Purpose: To create a program using python that can  accurately calculate the commission received by a salesperson at JamEx Limited.
+sales_person_number  =(input("please enter sale's person number"))
+if len(sales_person_number) != 4:
+        print("The salesperson's number must be 4 digits\n")
+else:
+        sales_person_number = int(sales_person_number)
 sales_amount=int(input("please enter sales amount"))
 class1  = int(input("please enter class"))
 if class1 == 1:
@@ -19,6 +24,6 @@ elif class1 == 2:
 elif class1 == 3:
     rate=0.045
 else:
-    print('Error!')
+    print('error occured')
 commission_rec = sales_amount*rate
 print(f'commission_rec= {commission_rec}')
